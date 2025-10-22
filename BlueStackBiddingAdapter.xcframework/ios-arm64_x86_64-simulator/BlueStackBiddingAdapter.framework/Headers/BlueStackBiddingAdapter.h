4804a78e72c0ec55a3f333c53d50d7cc294a79fc
sakibAzerion <shadman.sakib@ext.azerion.com>
2025-10-22 21:36:07 +0600

    Add podspec for bluestack bidding adapter

---
+++ BlueStackBiddingAdapter.xcframework/ios-arm64_x86_64-simulator/BlueStackBiddingAdapter.framework/Headers/BlueStackBiddingAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BlueStackBiddingAdapter.h
+//  BlueStackBiddingAdapter
+//
+//  Created by Bishnu Pada Chanda on 29/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlueStackBiddingAdapter.
+FOUNDATION_EXPORT double BlueStackBiddingAdapterVersionNumber;
+
+//! Project version string for BlueStackBiddingAdapter.
+FOUNDATION_EXPORT const unsigned char BlueStackBiddingAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueStackBiddingAdapter/PublicHeader.h>
+
+
