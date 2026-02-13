b1bc612c19e5bceaf2bd79d30d17463ce4fd1b5b
sakibAzerion <shadman.sakib@ext.azerion.com>
2026-02-13 12:03:08 +0600

    Updated the package.swift file

--- Package.swift
+++ Package.swift
@@ -1,57 +1,32 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BlueStackBiddingAdapter",
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "BlueStackBiddingAdapter",
             targets: ["BlueStackBiddingAdapterTarget"]
-        ),
-        .library(
-            name: "BlueStackBiddingAdapterBidding",
-            targets: ["BlueStackBiddingAdapterTarget"]
-        ),
-        .library(
-            name: "BlueStackBiddingAdapterPrebid",
-            targets: ["BlueStackPrebidAdapterTarget"]
         )
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/azerion/BlueStackSDK.git",
-            from: "5.4.0"
-        ),
-        .package(
-            url: "https://github.com/prebid/prebid-mobile-ios.git",
-            exact: "3.2.0"
-        )
+        .package(url: "https://github.com/azerion/BlueStackSDK.git", from: "5.4.0"),
+        .package(url: "https://github.com/prebid/prebid-mobile-ios.git", exact: "3.2.0")
     ],
     targets: [
         .target(
             name: "BlueStackBiddingAdapterTarget",
             dependencies: [
-                .target(name: "BlueStackBiddingAdapter"),
-                .target(name: "BlueStackPrebidAdapterTarget"),
-                .product(name: "BlueStackSDK", package: "BlueStackSDK")
+                .product(name: "PrebidMobile", package: "prebid-mobile-ios"),
+                .target(name: "BlueStackBiddingAdapter", condition: .when(platforms: [.iOS])),
+                .target(name: "BlueStackPrebidAdapter", condition: .when(platforms: [.iOS])),
+                .product(name: "BlueStackSDK", package: "BlueStackSDK", condition: .when(platforms: [.iOS]))
             ],
             path: "BlueStackBiddingAdapterWrapper"
         ),
-        
-        .target(
-            name: "BlueStackPrebidAdapterTarget",
-            dependencies: [
-                .target(name: "BlueStackPrebidAdapter"),
-                .product(name: "PrebidMobile", package: "prebid-mobile-ios")
-            ],
-            path: "BlueStackPrebidAdapterWrapper"
-        ),
-        
         .binaryTarget(
             name: "BlueStackBiddingAdapter",
             path: "BlueStackBiddingAdapter.xcframework"
