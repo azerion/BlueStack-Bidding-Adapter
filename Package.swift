e49705f0deca7c5dd809ad1b1eb7a49d661b8eca
sakibAzerion <shadman.sakib@ext.azerion.com>
2026-02-13 11:41:24 +0600

    Update the prebid version to 3.2.0

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/prebid/prebid-mobile-ios.git",
-            exact: "3.1.0"
+            exact: "3.2.0"
         )
     ],
     targets: [
